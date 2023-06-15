e34a759c870ab8ab7bb380ebcda0c66ad425d776
ryan pujo <pujotriantoro@gmail.com>
2023-06-15 12:01:35 +0700

    add some documentation

--- cmd/main.go
+++ cmd/main.go
@@ -7,10 +7,19 @@ import (
 )
 
 func main() {
-	action := flag.String("action", "sum", "action to apply to a certain column, default to sum operation")
+	action := flag.String("action", "sum", "action to apply to a certain column med|avg|sum")
 	col := flag.Int64("col", 1, "col to process default to column 1")
 
 	flag.Parse()
+
+	/*
+
+		use the available executable file named csv to run the program
+		example:
+
+		./path/to/csv -action [specify the action med|avg|sum] -col [specify the column] [specify csv file]
+
+	*/
 
 	if err := Run(flag.Args(), *action, *col, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
